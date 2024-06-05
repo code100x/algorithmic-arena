76758397b2c11580a8a26295fc7721235bffde00
harkirat <harkirat96@gmail.com>
2024-06-06 02:37:54 +0530

    fix problems

--- apps/problems/simple-array/boilerplate-full/function.js
+++ apps/problems/simple-array/boilerplate-full/function.js
@@ -1,8 +1,9 @@
 
 ##USER_CODE_HERE##
 
-const input = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n');
+const input = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n').join(' ').split(' ');
 const num1 = parseInt(input.shift());
   const num2 = parseInt(input.shift());
 const result = sum(num1, num2);
 console.log(result);
+    
