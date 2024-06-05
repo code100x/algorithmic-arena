0b91aa42da2da65457deac21b04a9f65e7b8112a
harkirat <harkirat96@gmail.com>
2024-06-06 02:36:23 +0530

    removed old code

--- apps/problem-submitter/src/index.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import v1Router from "./routes/v1";
-
-const app = express();
-app.use(express.json());
-
-app.use("/v1/", v1Router);
-
-app.listen(process.env.PORT || 3000);
--- apps/problem-submitter/src/types.ts
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-
-export const SubmissionInput = z.object({
-  code: z.string(),
-  languageId: z.enum(["js", "cpp", "rs"]),
-  problemId: z.string(),
-});
