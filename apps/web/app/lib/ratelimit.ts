83c4a2368e50786d6e32449897022df720617f54
ShubhamMewara <shubhammewara12@gmail.com>
2024-06-08 01:52:18 +0530

    updated ratelimit

--- apps/web/app/lib/ratelimit.ts
+++ apps/web/app/lib/ratelimit.ts
@@ -2,24 +2,13 @@ import Redis from 'ioredis';
 
 const redis = new Redis(process.env.REDIS_URL!);
 
-export async function isRequestAllowed(userId: string,timeWindow:number): Promise<boolean> {
+export async function isRequestAllowed(userId: string, timeWindow: number): Promise<boolean> {
     const currentTime = Math.floor(Date.now() / 1000);
     const key = `rate_limit:${userId}`;
-
     const maxRequests = 2;
-
-    const timestamps = await redis.lrange(key, 0, -1);
-    const validTimestamps = timestamps
-        .map(Number)
-        .filter(timestamp => timestamp > currentTime - timeWindow);
-
-    if (validTimestamps.length < maxRequests) {
-        await redis.multi()
-            .rpush(key, currentTime)
-            .expire(key, timeWindow)
-            .exec();
-        return true;
-    } else {
-        return false;
-    }
+    await redis.zremrangebyscore(key, '-inf', currentTime - timeWindow);
+    await redis.zadd(key, currentTime, currentTime);
+    const requestCount = await redis.zcard(key);
+    await redis.expire(key, timeWindow);
+    return requestCount <= maxRequests;
 }
