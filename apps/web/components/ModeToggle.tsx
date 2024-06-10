471e828dc6874cb4a2210e83f27ee875c7eb5e2b
dillu786 <azam.dilshad@gmail.com>
2024-06-11 01:12:01 +0530

    fixed themes

--- apps/web/components/ModeToggle.tsx
+++ apps/web/components/ModeToggle.tsx
@@ -13,12 +13,11 @@ import {
 } from "../../../packages/ui/src/@/components/ui/dropdown-menu"
 
 export function ModeToggle() {
-  const { setTheme } = useTheme()
-
+  const { resolvedTheme,setTheme } = useTheme()
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
+        <Button style={{color:resolvedTheme=='dark'?'white':'black'}} variant="outline" size="icon">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
