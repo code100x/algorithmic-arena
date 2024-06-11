814446642047eb94b4bb4cf84e8a4c5f18bd0c4d
Ankur1493 <ankursharma1493@gmail.com>
2024-06-12 01:44:41 +0530

    added a dynamic search bar to search problems

--- apps/web/components/LevelFilter.tsx
+++ apps/web/components/LevelFilter.tsx
@@ -8,9 +8,12 @@ import {
   SelectValue,
 } from "@repo/ui/select";
 import { useRouter } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 
 export const LevelFilter = () => {
   const router = useRouter();
+  const searchParams = useSearchParams();
+  const currentLevel = searchParams.get("level")
 
   const handleChange = (value: string) => {
     if (value === "none") {
@@ -23,10 +26,10 @@ export const LevelFilter = () => {
   return (
     <Select onValueChange={handleChange}>
       <SelectTrigger className="w-[180px]">
-        <SelectValue placeholder="Difficulty" />
+        <SelectValue placeholder={currentLevel ? currentLevel : "Select a Difficulty"} />
       </SelectTrigger>
       <SelectContent>
-        <SelectItem value="none">None</SelectItem>
+        <SelectItem value="none">Select a Difficulty</SelectItem>
         <SelectItem value="easy">Easy</SelectItem>
         <SelectItem value="medium">Medium</SelectItem>
         <SelectItem value="hard">Hard</SelectItem>
--- apps/web/components/Problems.tsx
+++ apps/web/components/Problems.tsx
@@ -1,6 +1,7 @@
 import { getProblems } from "../app/db/problem";
 import { ProblemCard } from "./ProblemCard";
 import { LevelFilter } from "./LevelFilter";
+import { SearchBar } from "./SearchBar";
 
 export async function Problems() {
   const problems = await getProblems();
@@ -9,6 +10,7 @@ export async function Problems() {
   return (
     <section className="bg-white dark:bg-gray-900 py-8 md:py-12 min-h-screen">
       <div className="w-full flex justify-center gap-4">
+        <SearchBar problems={problems} />
         <LevelFilter />
       </div>
       <div className="container mx-auto px-4 md:px-6">
