95f0793d6700141a399ca8292cd7a13e01335be7
Harsh Thakur <73895659+iharsh02@users.noreply.github.com>
2024-06-13 03:05:47 +0530

    Added Drop shadow to the profile sidebar  and user detail form

--- apps/web/components/Profile.tsx
+++ apps/web/components/Profile.tsx
@@ -7,12 +7,13 @@ import { authOptions } from "../app/lib/auth";
 export async function Profile() {
   const session = await getServerSession(authOptions);
   const user = session?.user;
+
   return (
-    <div className="flex flex-col lg:flex-row w-full">
-      <div className="hidden lg:block lg:w-3/12 mt-10 mx-5 p-5  dark:bg-slate-900 rounded-lg">
+    <div className="grid grid-cols-1 lg:grid-cols-12 gap-5 w-full p-5">
+      <div className="lg:col-span-3 p-5 dark:bg-slate-900 rounded-lg px-6 py-3 shadow-2xl shadow-shadow-500 hidden lg:block">
         <ProfileSideBar />
       </div>
-      <div className="w-full lg:w-9/12 mt-10 mx-5 p-5 dark:bg-slate-900 rounded-lg">
+      <div className="lg:col-span-9 dark:bg-slate-900 rounded-lg px-6 py-3 shadow-2xl shadow-shadow-500">
         <UserDetailForm user={user} />
       </div>
     </div>
