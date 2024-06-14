4fc6de25f81e6b1bf0dd38f7f4100b1d186a42a3
harkirat <harkirat96@gmail.com>
2024-06-14 06:29:28 +0530

    update migration

--- packages/db/prisma/migrations/20240612204650_added_relation/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[token]` on the table `submissions` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "submissions_token_key" ON "submissions"("token");
