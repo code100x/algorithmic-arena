d62e47c643bbf9f25ddbdb3de438b99bd7347434
Varun Deep Saini <varun.23bcs10048@ms.sst.scaler.com>
2024-07-05 23:00:53 +0530

    Added Better Testcases

--- apps/problems/Broken SubArrays/tests/python.py
+++ apps/problems/Broken SubArrays/tests/python.py
@@ -1,25 +1,46 @@
 import random
 import sys
 
-curr = 51
+curr = 91
 
 def gen_array_and_print(n, a, b):
-    print(n)
+    arr = []
     for i in range(n):
-        print(random.randint(a, b), end=" ")
-    print()
+        x = random.randint(a, b)
+        arr.append(x)
 
+    return arr
 
-for i in range(40):
+for i in range(1):
 
     filenamee= f"input/{curr+i}.txt"
     sys.stdout = open(filenamee, "w")
 
-    n = random.randint(1, 1e5)
+    n = 1e5
+    n = int(n)
+    # a = gen_array_and_print(n, 1, 1e4)
+    a = [1] * n
 
-    gen_array_and_print(n, 1, 1e4)
+    i = random.randint(0, n-1)
 
-    b = random.randint(1,1e6)
+    j = random.randint(i, n-1)
+
+    # sum of subarray[i,j]
+
+    print(n)
+
+    for k in a:
+        print(k, end=" ")
+    print()
+
+    sum = 0
+
+    for k in range(i, j+1):
+        sum += a[k]
+
+    
+
+    b = sum
 
     print(b)
 
