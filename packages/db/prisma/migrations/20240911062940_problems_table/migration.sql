92eaaba636c78b20085167b46053cd105f627879
myj009 <mohammad.jagora@deliveroo.com>
2024-09-11 12:01:02 +0530

    Updating db migrations

--- packages/db/prisma/migrations/20240911062940_problems_table/migration.sql
+++ packages/db/prisma/migrations/20240911062940_problems_table/migration.sql
@@ -2,7 +2,6 @@
   Warnings:
 
   - The values [AC] on the enum `SubmissionResult` will be removed. If these variants are still used in the database, this will fail.
-  - Added the required column `constraints` to the `Problem` table without a default value. This is not possible if the table is not empty.
   - Added the required column `languageId` to the `Submission` table without a default value. This is not possible if the table is not empty.
 
 */
@@ -18,7 +17,7 @@ ALTER TABLE "Submission" ALTER COLUMN "status" SET DEFAULT 'PENDING';
 COMMIT;
 
 -- AlterTable
-ALTER TABLE "Problem" ADD COLUMN     "constraints" TEXT NOT NULL,
+ALTER TABLE "Problem" ADD COLUMN     "constraints" TEXT,
 ADD COLUMN     "examples" TEXT[],
 ADD COLUMN     "points" INTEGER NOT NULL DEFAULT 100,
 ADD COLUMN     "topics" TEXT[];
