7003b1fdfb9f69cada6cd31592782963cf6e221e
jashandeep <jashandeep1659@gmail.com>
2024-09-17 21:37:52 +0530

    fix: auth system fix

--- apps/web/app/admin/layout.tsx
+++ apps/web/app/admin/layout.tsx
@@ -1,9 +1,13 @@
 import { getServerSession } from "next-auth";
 import React from "react";
+import { authOptions } from "../lib/auth";
+import { redirect } from "next/navigation";
 
 const layout = async ({ children }: { children: React.ReactNode }) => {
-  const session = await getServerSession();
-  console.log(session?.user);
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user || session.user.role !== "ADMIN") {
+    redirect("/");
+  }
   return <div>{children}</div>;
 };
 
